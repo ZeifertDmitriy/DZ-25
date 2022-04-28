ee437ba2dff05b5f3a8f39e8f087db5e7df8d049
Dmitriy <zeifd@mail.ru>
2022-04-29 00:50:22 +0700

    Домашнее задание № 27

--- dz_27/Program.cs
+++ dz_27/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int SumNum(int N)
+{
+    int i = 1;
+    int sum = 0;
+    while (N > 10)
+    {
+        sum += N % 10;
+        N = N / 10;
+        i++;
+    }
+    sum += N % 10;
+    return sum;
+}
+
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine(SumNum(N));
