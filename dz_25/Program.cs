b8895b328f6b0649464d79dbc529aae9eb94dca7
Dmitriy <zeifd@mail.ru>
2022-04-29 00:34:31 +0700

    Домашнее задание № 25

---
+++ dz_25/Program.cs
@@ -0,0 +1,14 @@
+﻿int Res(int N, int S)
+{
+    int r = N;
+    for (int i = 2; i <= S; i++)
+    {
+        r *= N;
+    }
+    return r;
+}
+Console.WriteLine("Введите число А: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число В: ");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine(Res(A, B));
