d5c0cada88cb76a663e8b20b21a7d35678130268
Dmitriy <zeifd@mail.ru>
2022-04-29 01:12:35 +0700

    Домашнее задание № 29

---
+++ dz_29/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int[] mass = new int[8];
+Console.Write("[");
+int i = 0;
+for (; i < (mass.Length - 1); i++)
+{
+    mass[i] = new Random().Next();
+    Console.Write($"{mass[i]}, ");
+}
+mass[i] = new Random().Next();
+Console.Write($"{mass[i]}]");
